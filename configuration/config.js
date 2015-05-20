805432d47ce5ff16f22a41dbbdd08b8728fb18bb
Shan1024 <gambit1024@gmail.com>
2015-05-20 23:44:49 +0530

    First commit

---
+++ configuration/config.js
@@ -0,0 +1,15 @@
+module.exports={
+/*
+* This file contains the configurations information of Twitter login app.
+* It consists of Twitter app information, database information.
+*/
+
+	"facebook_api_key" 		: 			"819814544764038",
+	"facebook_api_secret"	:				"d8817a92f941ca5f80068dedd7c4a9f6",
+	"callback_url"				:				"http://localhost:3000/auth/facebook/callback",
+	"use_database"				:				"false",
+	"host"								:				"localhost",
+	"username"						:				"root",
+	"password"						:				"",
+	"database"						:				""
+}
